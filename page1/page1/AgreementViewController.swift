3349d595e24e54f0be357e164efa11c809f28581
P.J. Finlay <caitlinstanton44@gmail.com>
2017-03-07 15:41:19 -0500

    multiple views for launching of app

--- page1/page1/AgreementViewController.swift
+++ page1/page1/AgreementViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AgreementViewController: UIViewController {
     
     private let SCREENSIZE = UIScreen.main.bounds
     var participationForm: UITextView!
+    var instructionLabel: UILabel!
     var agreementCheckBox: UIButton!
+    var nameLabel: UILabel!
     var nameTextField: UITextField!
     var continueButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,24 +36,25 @@ class ViewController: UIViewController {
         participationForm.layer.borderColor = UIColor.black.cgColor
         participationForm.backgroundColor = UIColor.gray
         participationForm.isUserInteractionEnabled = false
+        participationForm.isScrollEnabled = true
         view.addSubview(participationForm)
         
-        /*
-         agreementCheckBox = UIButton(frame: CGRect(x:SCREENSIZE.width/2-5, y:350, width:10, height:10))
-         agreementCheckBox.backgroundColor = UIColor.blue
-         agreementCheckBox.addTarget(self, action: #selector(btn_box(sender:)), for: .touchUpInside)
-         view.addSubview(agreementCheckBox)
-         */
+        instructionLabel = UILabel(frame: CGRect(x:30, y:380, width:SCREENSIZE.width-60, height:60))
+        instructionLabel.text = "I have fully read and understood the agreement above"
+        instructionLabel.lineBreakMode = .byWordWrapping // or NSLineBreakMode.ByWordWrapping
+        instructionLabel.numberOfLines = 0
+        instructionLabel.textAlignment = .center
+        view.addSubview(instructionLabel)
         
-        let checkBoxLabel = UILabel(frame: CGRect(x:30, y:380, width:SCREENSIZE.width-60, height:60))
-        checkBoxLabel.text = "I have fully read and understood the agreement above"
-        checkBoxLabel.lineBreakMode = .byWordWrapping // or NSLineBreakMode.ByWordWrapping
-        checkBoxLabel.numberOfLines = 0
-        checkBoxLabel.textAlignment = .center
-        view.addSubview(checkBoxLabel)
+        agreementCheckBox = UIButton(frame: CGRect(x:SCREENSIZE.width/2-50, y:450, width:100, height:20))
+        agreementCheckBox.setTitle("I Agree", for: .normal)
+        agreementCheckBox.backgroundColor = UIColor.blue
+        agreementCheckBox.addTarget(self, action: #selector(makeSignatureAppear), for: .touchUpInside)
+        view.addSubview(agreementCheckBox)
         
-        let nameLabel = UILabel(frame: CGRect(x:30, y:450, width:80, height:30))
+        nameLabel = UILabel(frame: CGRect(x:30, y:450, width:80, height:30))
         nameLabel.text = "Full Name"
+        nameLabel.isHidden = true
         view.addSubview(nameLabel)
         
         nameTextField = UITextField(frame: CGRect(x:120, y:450, width:SCREENSIZE.width-160, height:30))
@@ -58,30 +62,23 @@ class ViewController: UIViewController {
         nameTextField.layer.masksToBounds = true
         nameTextField.layer.borderColor = UIColor.gray.cgColor
         nameTextField.layer.borderWidth = 2.0
+        nameTextField.isHidden = true
         view.addSubview(nameTextField)
         
         continueButton = UIButton(frame: CGRect(x:SCREENSIZE.width/2-50, y:550, width:100, height:20))
         continueButton.setTitle("Continue", for: .normal)
         continueButton.backgroundColor = UIColor.blue
+        continueButton.isHidden = true
         view.addSubview(continueButton)
     }
     
-    /*
-     @IBAction func btn_box(sender: UIButton) {
-     if (btn_box.UIControlStateNormal == true)
-     {
-     btn_box.setBackgroundImage(UIImage(named: "box"), forState: UIControlState.Normal)
-     
-     btn_box.selected = false;
-     }
-     else
-     {
-     btn_box.setBackgroundImage(UIImage(named: "checkBox"), forState: UIControlState.Normal)
-     
-     btn_box.selected = true;
-     }
-     }
-     */
+    func makeSignatureAppear(){
+        instructionLabel.text = "Enter your full name to confirm and continue"
+        agreementCheckBox.isHidden = true
+        nameLabel.isHidden = false
+        nameTextField.isHidden = false
+        continueButton.isHidden = false
+    }
     
     
 }
---
+++ page1/page1/FirstViewController.swift
@@ -0,0 +1,61 @@
+//
+//  FirstViewController.swift
+//  page1
+//
+//  Created by lrc on 3/7/17.
+//  Copyright © 2017 lrc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    private let SCREENSIZE = UIScreen.main.bounds
+    var welcomeLabel: UILabel!
+    var continueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.lightGray
+        addUIElements()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0, execute: {
+            self.showAgreementViewControllerModal()
+        })
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func addUIElements() {
+        welcomeLabel = UILabel(frame: CGRect(x:30, y:380, width:SCREENSIZE.width-60, height:60))
+        welcomeLabel.text = NSLocalizedString("welcome_message", comment: "")
+        welcomeLabel.lineBreakMode = .byWordWrapping // or NSLineBreakMode.ByWordWrapping
+        welcomeLabel.numberOfLines = 0
+        welcomeLabel.textAlignment = .center
+        view.addSubview(welcomeLabel)
+    }
+    
+    func showAgreementViewControllerModal() {
+        //pull up RedViewController with modal transition, by pulling the view up from the bottom
+        let agreementViewController = AgreementViewController()
+        present(agreementViewController, animated: true) {
+            //code executed after redViewController is presented
+            print("good job")
+        }
+    }
+    
+    func showAgreementViewControllerPush() {
+        let agreementViewController = AgreementViewController()
+        if let navController = self.navigationController {
+            navController.pushViewController(agreementViewController, animated: true)
+        }
+        //navigationController?.pushViewController(redViewController, animated: true)
+    }
+
+    
+}
