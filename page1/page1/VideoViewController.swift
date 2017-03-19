41efb3827c396d81892ff7bc8d2468664919eaf3
P.J. Finlay <caitlinstanton44@gmail.com>
2017-03-19 17:42:08 -0400

    video view controller

--- page1/page1/VideoViewController.swift
+++ page1/page1/VideoViewController.swift
@@ -7,6 +7,80 @@
 //
 
 import UIKit
+import MobileCoreServices
+
+class VideoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    func record(sender: AnyObject) {
+        startCameraFromViewController(viewController: self, withDelegate: self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate)
+    }
+    
+    func startCameraFromViewController(viewController: UIViewController, withDelegate delegate: protocol<UIImagePickerControllerDelegate, UINavigationControllerDelegate>) -> Bool {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) == false {
+            return false
+        }
+        
+        let cameraController = UIImagePickerController()
+        cameraController.sourceType = .camera
+        cameraController.mediaTypes = [kUTTypeMovie as NSString as String]
+        cameraController.allowsEditing = false
+        cameraController.delegate = delegate
+        
+        present(cameraController, animated: true, completion: nil)
+        return true
+    }
+    
+    func video(videoPath: NSString, didFinishSavingWithError error: NSError?, contextInfo info: AnyObject) {
+        var title = "Success"
+        var message = "Video was saved"
+        if let _ = error {
+            title = "Error"
+            message = "Video failed to save"
+        }
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+// MARK: - UIImagePickerControllerDelegate
+
+extension VideoViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        dismiss(animated: true, completion: nil)
+        // Handle a movie capture
+        if mediaType == kUTTypeMovie {
+            guard let path = (info[UIImagePickerControllerMediaURL] as! NSURL).path else { return }
+            if UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path) {
+                //UISaveVideoAtPathToSavedPhotosAlbum(path, self, #selector(RecordVideoViewController.video(_:didFinishSavingWithError:contextInfo:)), nil)
+            }
+        }
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+
+extension VideoViewController: UINavigationControllerDelegate {
+}
+
+
+/*
+import UIKit
 
 class VideoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -83,3 +157,4 @@ class VideoViewController: UIViewController, UIImagePickerControllerDelegate, UI
     }
     
 }
+ */
