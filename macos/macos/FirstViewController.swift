a3672fa8832d465d9dbf67f9cdafdcbf764a8961
Caitlin Stanton <caitlinstanton44@gmail.com>
2017-04-11 15:29:23 -0400

    new macos app

---
+++ macos/macos/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  macos
+//
+//  Created by Caitlin Stanton on 4/11/17.
+//  Copyright © 2017 Caitlin Stanton. All rights reserved.
+//
+
+import Cocoa
+
+class FirstViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ macos/macos/VideoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VideoViewController.swift
+//  macos
+//
+//  Created by Caitlin Stanton on 4/11/17.
+//  Copyright © 2017 Caitlin Stanton. All rights reserved.
+//
+
+import Cocoa
+
+class VideoViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
