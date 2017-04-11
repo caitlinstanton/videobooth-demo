e7cf2b9fa823d63ef4bd612a5949e112cd1d851f
Caitlin Stanton <caitlinstanton44@gmail.com>
2017-04-11 16:21:48 -0400

    accessed video camera

--- macos/macos/VideoViewController.swift
+++ macos/macos/VideoViewController.swift
@@ -7,12 +7,62 @@
 //
 
 import Cocoa
+import AVFoundation
+import AVKit
+import QuartzCore
+
 
 class VideoViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
     }
     
+    @IBAction func recordButtonPressed(_ sender: NSButton) {
+        //let view = NSView(frame: NSRect(x: 0.0, y: 0.0, width: 640.0, height: 480.0))
+        
+        let session = AVCaptureSession()
+        
+        session.sessionPreset = AVCaptureSessionPreset640x480
+        session.beginConfiguration()
+        session.commitConfiguration()
+        
+        var input : AVCaptureDeviceInput
+        if let devices : [AVCaptureDevice] = AVCaptureDevice.devices() as? [AVCaptureDevice] {
+            for device in devices {
+                if device.hasMediaType(AVMediaTypeVideo) && device.supportsAVCaptureSessionPreset(AVCaptureSessionPreset640x480) {
+                    do {
+                        input = try AVCaptureDeviceInput(device: device as AVCaptureDevice) as AVCaptureDeviceInput
+                        
+                        if session.canAddInput(input) {
+                            session.addInput(input)
+                            break
+                        }
+                    }
+                    catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            let output = AVCaptureVideoDataOutput()
+            output.videoSettings = [kCVPixelBufferPixelFormatTypeKey as AnyHashable: Int(kCVPixelFormatType_32BGRA)]
+            output.alwaysDiscardsLateVideoFrames = true
+            
+            if session.canAddOutput(output) {
+                session.addOutput(output)
+            }
+            
+            let captureLayer = AVCaptureVideoPreviewLayer(session: session)
+            view.wantsLayer = true
+            view.layer = captureLayer
+            
+            session.startRunning()
+            
+            //View -> Assistant Editor -> Show Assistant Editor
+            //XCPlaygroundPage.currentPage.liveView = view
+        }
+   
+    }
 }
