4652053e51b044b53f3d6625090e2d2c4454d70f
Caitlin Stanton <caitlinstanton44@gmail.com>
2017-04-11 13:49:17 -0400

    new macos app

---
+++ macos/macos/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  macos
+//
+//  Created by Caitlin Stanton on 4/11/17.
+//  Copyright © 2017 Caitlin Stanton. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
