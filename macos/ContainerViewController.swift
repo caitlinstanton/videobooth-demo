df0b71bfe0ae0710405e92eaf374541938217d1c
P.J. Finlay <pjappdevelopment@gmail.com>
2017-04-23 21:57:42 -0400

    macos view transitions

---
+++ macos/ContainerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContainerViewController.swift
+//  macos
+//
+//  Created by lrc on 4/23/17.
+//  Copyright © 2017 Caitlin Stanton. All rights reserved.
+//
+
+import Cocoa
+
+class ContainerViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ macos/views/views/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  views
+//
+//  Created by lrc on 4/23/17.
+//  Copyright © 2017 lrc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
